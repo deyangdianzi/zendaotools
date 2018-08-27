a723ad9f4ae425fe6e4940d79fe1202484163a7f
杜大江 <dudajiang@boco.com.cn>
2018-08-27 15:10:00 +0800

    去除特别处理

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -1598,7 +1598,7 @@ $lang->story->storyValueLevelList['E']         = 'E:0-2（不含2）';
       sprint = sprintid
     }
 
-    sprint = '20180826';
+    // sprint = '20180826';
 
     let modelczd = this.model('');
     let sql1 = 'SELECT defamb,ratioofsprintonline,ratioofreqverify,ratioofcompclose,reqlongunclose,buglongunclose FROM zentao.bi_report_amb_sprintbbs  where level = 4 and sprint = ' + sprint;
@@ -1680,8 +1680,8 @@ $lang->story->storyValueLevelList['E']         = 'E:0-2（不含2）';
       sprintbefor = sprintobj[2].release_alias;
     }
 
-    sprint = '20180826';
-    sprintbefor = '20180729';
+    // sprint = '20180826';
+    // sprintbefor = '20180729';
 
     console.log('开始进行本迭代的指标计算', sprint, sprintbefor);
 
